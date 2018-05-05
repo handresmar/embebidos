5e10d5ef5b138a52e161ec69b1c92969053e61f7
chacal1231 <jedmacmahonve@unal.edu.co>
2018-05-04 19:10:39 -0500

    Terminado procesamiento de NMEA en Latitud y Longitud

--- servidor.py
+++ servidor.py
@@ -3,7 +3,6 @@ import time
 #Serial setup
 ser_sigfox = serial.Serial(port='/dev/ttyUSB0', baudrate=9600,timeout=0)
 ser_sam = serial.Serial(port='/dev/ttySP0', baudrate=115200, timeout=0)
-#Serial write
 #x = "0000,1"
 #ser_sigfox.write("AT$RC\r")
 #ser_sigfox.write("AT$SF=")
@@ -12,14 +11,24 @@ ser_sam = serial.Serial(port='/dev/ttySP0', baudrate=115200, timeout=0)
 #ans="answer is: "+ser_sigfox.read(ser_sigfox.inWaiting())+" sigfox"           $
 #print ans
 while 1:
-        sam = ser_sam.readline()
-        if (sam.find("VAL=N")==0 or sam.find("LAT=040518")==0):
-        	print "Sin satelites"
-        elif (sam.find("VAL=A")==0):
-         	if (sam.find("LAT=")==0):
-        		LAT_tem = float(sam[4:])
-                DD = int(LAT_tem/100)
-                SS = float(LAT_tem) - DD * 100 
-                LAT = DD + SS/60
-                print "LAT=" + str(LAT)
-pass	
+	 	if (ser_sam.inWaiting()>0):
+                	sam = ser_sam.readline()
+                	Values = sam.split(";")
+                	if(Values[0]=="LAT=040518"):
+                        	print "Sin sateliles"
+               		if(Values[5]=="VAL=A"):
+                        	LAT_tem = float(Values[0][4:])                          
+                        	DD = int(LAT_tem/100)
+                        	SS = float(LAT_tem) - DD * 100
+                        	LAT = DD + SS/60
+                        	if(Values[0]=="CLAT=S"):
+                        		LAT = -LAT                          
+                       		LON_tem = float(Values[2][4:])                          
+                        	DD = int(LON_tem/100)
+                        	SS = float(LON_tem) - DD * 100                          
+                        	LON = DD + SS/60
+                        	if (Values[3]=="CLON=W"):
+					LON = -LON
+				Vel_Km = float(Values[4][3:]) * 1.852
+                        	print "LAT=" + str(round(LAT,6)) + " LON=" + str(round(LON,6)) + " Vel=" + str(round(Vel_Km,2))
+pass

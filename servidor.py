4319c8d1da0d75086b22d2cae23a46f8e8d36fde
chacal1231 <jedmacmahonve@unal.edu.co>
2018-05-06 12:43:44 -0500

    añadido estado del MPU al frenar

--- servidor.py
+++ servidor.py
@@ -14,7 +14,7 @@ while 1:
 	 	if (ser_sam.inWaiting()>0):
                 	sam = ser_sam.readline()
                 	Values = sam.split(";")
-                	if(Values[0]=="LAT=040518"):
+                	if(Values[0]=="LAT=040518" or Values[5]=="VAL=V"):
                         	print "Sin sateliles"
                		if(Values[5]=="VAL=A"):
                         	LAT_tem = float(Values[0][4:])                          
@@ -30,5 +30,5 @@ while 1:
                         	if (Values[3]=="CLON=W"):
 					LON = -LON
 				Vel_Km = float(Values[4][3:]) * 1.852
-                        	print "LAT=" + str(round(LAT,6)) + " LON=" + str(round(LON,6)) + " Vel=" + str(round(Vel_Km,2))
+                        	print "LAT=" + str(round(LAT,6)) + " LON=" + str(round(LON,6)) + " Vel=" + str(round(Vel_Km,2))  + " " + Values[6][0:5]
 pass

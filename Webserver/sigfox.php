58b6118f1942e70e164f72df76fe72080a2d91f6
David McMahon <jedmacmahonve@unal.edu.co>
2018-05-20 21:53:57 -0400

    Tramas desencriptadas, organizadas e insertadas en base de datos

--- Webserver/sigfox.php
+++ Webserver/sigfox.php
@@ -18,5 +18,24 @@ $result     =   mysqli_query($link,"SELECT * FROM tramas ORDER BY id DESC");
 $row        =   mysqli_fetch_array($result);            
 $id         =   ($row["id"]+1);
 mysqli_query($link,"INSERT INTO tramas(id,tramas,fecha) VALUES('$id','$_data','$_date')");
-
-
+//Data
+if(strpos($_data,"46494e")!==false){
+	$QueryData=mysqli_query($link,"SELECT * FROM tramas ORDER BY id DESC LIMIT 3");
+	$RowData=mysqli_fetch_array($QueryData);
+	foreach($QueryData as $RowData => $field){
+		if(strpos($field['tramas'],"494e49")!==false){
+                $trama1=str_replace("494e49","",$field['tramas']);
+        	}
+        	else if(strpos($field['tramas'],"46494e")!==false){
+                $trama3=str_replace("46494e","",$field['tramas']);
+        	}else{
+                $trama2=$field['tramas'];
+        	}
+}
+$mensaje = hex2bin($trama1 . $trama2 . $trama3);
+list($lat,$lon,$sp,$mpu) = explode(';', $mensaje);
+$result     =   mysqli_query($link,"SELECT * FROM gps ORDER BY id DESC");
+$row        =   mysqli_fetch_array($result);
+$id         =   ($row["id"]+1);
+mysqli_query($link,"INSERT INTO gps(id,fecha,lat,lon,sp,mpu) VALUES ('$id','$_date','$lat','$lon','$sp','$mpu')");
+}
